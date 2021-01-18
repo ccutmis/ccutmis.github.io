b423a6df4e641fc05533795b0f2273f8f9f31f47
aloha3307 <ccutmis@gmail.com>
2021-01-18 13:27:39 +0800

    ...

--- study-coding/js/page-ext.js
+++ study-coding/js/page-ext.js
@@ -6,9 +6,9 @@ $.ajax({
                 //$(".text").html(data);
 				//console.log(marked(data));
 				$("#md_content").html(marked(data));
-                $("#md_content").append("<div class='pb-5 text-center'><button type='button' class='btn btn-secondary' onclick='window.scrollTo(0,0);'>回到上方</button> <button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一頁</button></div>");
+                $("#md_content").append("<div class='pb-5 text-center'><button type='button' class='btn btn-secondary' onclick='window.scrollTo(0,0);'>回到上方</button> <button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一層</button></div>");
                 $("img").addClass("lazy img-fluid d-block m-2 mx-auto").lazyload();
-                $("#md_content h1").addClass("text-info mb-0 p-2 text-center").append("<button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一頁</button>");
+                $("#md_content h1").addClass("text-info mb-0 p-2 text-center").append("<button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一層</button>");
                 $("#md_content h3").addClass("text-warning mb-0 p-2");
                 $("#md_content p,#md_content ol li,#md_content ol li").addClass("text-info p-3");
                 $("blockquote p").removeClass("text-info").addClass("bg-dark text-white-50 mx-3 rounded");
--- study-coding/js/sub-index-ext.js
+++ study-coding/js/sub-index-ext.js
@@ -4,9 +4,9 @@ $.ajax({
             dataType: "text",
             success : function (data) {
                 $("#md_content").html(marked(data));
-                $("#md_content").append("<div class='pb-5 text-center'><button type='button' class='btn btn-secondary' onclick='window.scrollTo(0,0);'>回到上方</button> <button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一頁</button></div>");
+                $("#md_content").append("<div class='pb-5 text-center'><button type='button' class='btn btn-secondary' onclick='window.scrollTo(0,0);'>回到上方</button> <button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一層</button></div>");
                 $("img").addClass("lazy img-fluid d-block m-2 mx-auto").lazyload();
-                $("#md_content h1").addClass("text-info mb-0 p-2 text-center").append("<button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一頁</button>");
+                $("#md_content h1").addClass("text-info mb-0 p-2 text-center").append("<button type='button' class='btn btn-primary' onclick='window.location.href=\""+LAST_URL+"\";'>回上一層</button>");
                 $("#md_content h3").addClass("text-warning mb-0 p-2");
                 $("#md_content h3 a").addClass("btn btn-primary");
                 $("#md_content p,#md_content ol li,#md_content ol li").addClass("text-info p-3");
